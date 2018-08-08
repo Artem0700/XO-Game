5fd20406010602b254c62855e3eb86639f95b670
Artem0700 <07postmail@gmail.com>
2018-08-08 22:47:50 +0600

    invalids are created

--- XO-GameProject/src/exceptions/InvalidPointException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class InvalidPointException extends AbstractXOException {
-	
-}
--- XO-GameProject/src/io/game/xo/model/Field.java
+++ XO-GameProject/src/io/game/xo/model/Field.java
@@ -2,6 +2,9 @@ package io.game.xo.model;
 
 import java.awt.Point;
 
+import exceptions.AlreadyOccupiedException;
+import exceptions.InvalidePointException;
+
 public class Field {
 	private static final int FIELD_SIZE = 3;
 	private static final int MIN_COORDINATE = 0;
@@ -10,16 +13,25 @@ public class Field {
 	public int getSize() {
 		return FIELD_SIZE;
 	}
-	public Figure getFigure(final Point point) {
+	public Figure getFigure(final Point point) throws InvalidePointException {
+		if (!checkPoint(point)) {
+			throw new InvalidePointException();
+		}
 		return field[point.x][point.y];
 	}
-	public void setFigure(final Point point, final Figure figure) {
+	public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException {
+		if (!checkPoint(point)) {
+			throw new InvalidePointException();
+		}
+		if (field [point.x][point.y] != null) {
+			throw new AlreadyOccupiedException();
+		}
 		field [point.x][point.y] = figure;
 	}
 	private boolean checkPoint(final Point point) {
 		return checkCoordinate(point.x) && checkCoordinate(point.y);
 	}
 	private boolean checkCoordinate(final int coordinate) {
-		return coordinate >= MIN_COORDINATE && coordinate < FIELD_SIZE;
+		return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
 	}
 }
