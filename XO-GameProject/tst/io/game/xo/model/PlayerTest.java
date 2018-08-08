bf08decb659ebe656ab4b459e8447965e845ba49
Artem0700 <07postmail@gmail.com>
2018-08-08 21:49:38 +0600

    PlayerTest was created

--- XO-GameProject/tst/io/game/xo/model/PlayerTest.java
+++ XO-GameProject/tst/io/game/xo/model/PlayerTest.java
@@ -7,18 +7,21 @@ import org.junit.Test;
 public class PlayerTest {
 
 	@Test
-	public void testPlayer() {
-		fail("Not yet implemented");
+	public void testGetName() throws Exception {
+		final String inputValue = "Slava";
+		final String expectedValue = inputValue;
+		final Player player = new Player("Slava", null);
+		final String actualValue = player.getName();
+		assertEquals(expectedValue, actualValue);
 	}
 
 	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFigure() {
-		fail("Not yet implemented");
+	public void testGetFigure() throws Exception {
+		final Figure inputValue = Figure.X;
+		final Figure expectedValue = inputValue;
+		final Player player = new Player(null, inputValue);
+		final Figure actualValue = player.getFigure();
+		assertEquals(expectedValue, actualValue);
 	}
 
 }
