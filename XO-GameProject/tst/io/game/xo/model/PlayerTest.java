e5b2219f3aaffab6fbb908e554dfaea8e4b36bb6
Artem0700 <07postmail@gmail.com>
2018-08-08 20:58:03 +0600

    Player was created

---
+++ XO-GameProject/tst/io/game/xo/model/PlayerTest.java
@@ -0,0 +1,24 @@
+package io.game.xo.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFigure() {
+		fail("Not yet implemented");
+	}
+
+}
