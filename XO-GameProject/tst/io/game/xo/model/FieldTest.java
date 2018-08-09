e1aee9aaeb620bb56b236ad97d65953ec1258501
Artem0700 <07postmail@gmail.com>
2018-08-09 10:45:55 +0600

    all tests are passed

--- XO-GameProject/tst/io/game/xo/model/FieldTest.java
+++ XO-GameProject/tst/io/game/xo/model/FieldTest.java
@@ -1,21 +1,43 @@
 package io.game.xo.model;
-
 import static org.junit.Assert.*;
-
 import java.awt.Point;
-
 import org.junit.Test;
 
+import exceptions.AlreadyOccupiedException;
 import exceptions.InvalidePointException;
-
 public class FieldTest {
-
 	@Test
 	public void testGetSize() throws Exception {
 		final Field field = new Field();
 		assertEquals(3, field.getSize());
 	}
-
+	@Test
+	public void testSetFigure() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0, 0);
+		final Figure inputFigure = Figure.O;
+		field.setFigure(inputPoint, inputFigure);
+		final Figure actualFigure = field.getFigure(inputPoint);
+		assertEquals(inputFigure, actualFigure);
+	}
+	@Test
+	public void testSetFigureWhenAlreadyOccupied() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0, 0);
+		final Figure inputFigure = Figure.O;
+		field.setFigure(inputPoint, inputFigure);
+		try {
+			field.setFigure(inputPoint, inputFigure);
+			fail();
+		} catch (final AlreadyOccupiedException e) {}
+	}
+	@Test
+	public void testGetFigureWhenFigureIsNotSet() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0, 0);
+		final Figure actualFigure = field.getFigure(inputPoint);
+		assertNull(actualFigure);
+	}
 	@Test
 	public void testGetFigureWhenXIsLessThanZero() {
 		final Field field = new Field();
