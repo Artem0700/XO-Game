a07c921fd582dc67e000f61e2baeae97f39c9865
Artem0700 <07postmail@gmail.com>
2018-08-09 13:27:46 +0600

    Field changes (rules part have been removed)

--- XO-GameProject/src/controllers/MoveController.java
+++ XO-GameProject/src/controllers/MoveController.java
@@ -1,7 +1,18 @@
 package controllers;
+import java.awt.Point;
 
+import exceptions.AlreadyOccupiedException;
+import exceptions.InvalidePointException;
+import io.game.xo.model.Field;
+import io.game.xo.model.Figure;
 public class MoveController {
-	public void applyFigure() {
-		smt
+	public void applyFigure(final Field field, 
+							final Figure figure, 
+							final Point point) throws InvalidePointException, 
+													  AlreadyOccupiedException {
+		if (field.getFigure(point) != null) {
+			throw new AlreadyOccupiedException();
+		}
+		field.setFigure(point, figure);
 	}
 }
